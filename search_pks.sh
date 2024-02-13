f4784ad3eeac72c8452de0aab267b07bab4ead88
Marco Galardini <marco.galardini@twincore.de>
2024-02-13 16:52:33 +0100

    Last PKS touch

--- search_pks.sh
+++ search_pks.sh
@@ -1,3 +1,4 @@
 for i in $(ls data/gffs); do name=$(basename $i .gff); echo $name;python workflow/scripts/gff2faa.py data/gffs/$i data/fastas/$name.fasta > data/faas/$name.faa; done
 python workflow/scripts/extract_pks.py data/pks.gb > out/pks.faa
 for i in $(ls data/faas); do name=$(basename $i .faa); blastp -query out/pks.faa -subject data/faas/$i -evalue 1E-4 -outfmt "6 qseqid sseqid pident qlen slen length nident evalue" > out/pks/$name.tsv; echo $name; done
+grep ">" data/faas/*.faa > notebooks/pids.txt
